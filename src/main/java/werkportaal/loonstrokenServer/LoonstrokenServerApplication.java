7858a52c44961714c456a433b1a3f3f69d02aae5
mohamedikhlaf27 <61310252+mohamedikhlaf27@users.noreply.github.com>
2022-04-13 15:21:28 +0200

    update LoonstrokenServerApplication.java

--- src/main/java/werkportaal/loonstrokenServer/LoonstrokenServerApplication.java
+++ src/main/java/werkportaal/loonstrokenServer/LoonstrokenServerApplication.java
@@ -1,10 +1,8 @@
 package werkportaal.loonstrokenServer;
 
 import werkportaal.loonstrokenServer.repository.LoonstrokenRepository;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -14,11 +12,4 @@ public class LoonstrokenServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LoonstrokenServerApplication.class, args);
 	}
-
-	// This is so that SimpleMessageConverter only String, byte[] and Serializable payloads supports.
-	@Bean
-	public Jackson2JsonMessageConverter jsonConverter() {
-		return new Jackson2JsonMessageConverter();
-	}
-
 }
