142c54d148fb12d470f6eacee956f61ae7c754f0
mohamedikhlaf27 <61310252+mohamedikhlaf27@users.noreply.github.com>
2022-04-13 15:49:05 +0200

    add project

---
+++ src/main/java/Werkportaal/loonstrokenserver/LoonstrokenServerApplication.java
@@ -0,0 +1,13 @@
+package Werkportaal.loonstrokenserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoonstrokenServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoonstrokenServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Werkportaal/loonstrokenserver/LoonstrokenServerApplicationTests.java
@@ -0,0 +1,13 @@
+package Werkportaal.loonstrokenserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoonstrokenServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
