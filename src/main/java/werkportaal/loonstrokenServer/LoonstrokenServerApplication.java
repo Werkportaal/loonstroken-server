642cf34aff94bdc43b40784c04885f32ea3eb344
mohamedikhlaf27 <61310252+mohamedikhlaf27@users.noreply.github.com>
2022-04-13 15:16:00 +0200

    add pipeline

--- src/main/java/werkportaal/loonstrokenServer/LoonstrokenServerApplication.java
+++ src/main/java/werkportaal/loonstrokenServer/LoonstrokenServerApplication.java
@@ -1,6 +1,6 @@
 package werkportaal.loonstrokenServer;
 
-import werkportaal.loonstrokenServer.repository.TemplateRepository;
+import werkportaal.loonstrokenServer.repository.LoonstrokenRepository;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackageClasses = TemplateRepository.class)
+@EnableJpaRepositories(basePackageClasses = LoonstrokenRepository.class)
 public class LoonstrokenServerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/werkportaal/loonstrokenServer/runner/DataLoaderRunner.java
+++ src/main/java/werkportaal/loonstrokenServer/runner/DataLoaderRunner.java
@@ -1,51 +1,51 @@
-package werkportaal.loonstrokenServer.runner;
-
-import werkportaal.loonstrokenServer.model.Template;
-import werkportaal.loonstrokenServer.repository.TemplateRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.stereotype.Component;
-
-@Component
-public class DataLoaderRunner implements ApplicationRunner {
-
-    @Value("${database.seed}")
-    private boolean seedDatabase;
-
-
-    Logger logger = LoggerFactory.getLogger(DataLoaderRunner.class);
-
-    /* Add repos here and remove templateRepository */
-    TemplateRepository templateRepository;
-
-
-    public DataLoaderRunner(TemplateRepository templateRepository){
-        this.templateRepository = templateRepository;
-    }
-
-
-    private boolean shouldSeed(){
-        return templateRepository.findAll().size() <= 0;
-    }
-
-
-    @Override
-    public void run(ApplicationArguments args)  {
-        if(!seedDatabase || !shouldSeed())
-        {
-            logger.info("Won't seed database, .env property is false or already seeded!");
-            return;
-        }
-
-        logger.info("Will seed database now!");
-
-        Template newTemplate = new Template();
-        newTemplate.setValue("setBySeed!");
-        templateRepository.save(newTemplate);
-
-    }
-
-}
+//package werkportaal.loonstrokenServer.runner;
+//
+//import werkportaal.loonstrokenServer.model.Loonstroken;
+//import werkportaal.loonstrokenServer.repository.LoonstrokenRepository;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.ApplicationArguments;
+//import org.springframework.boot.ApplicationRunner;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class DataLoaderRunner implements ApplicationRunner {
+//
+//    @Value("${database.seed}")
+//    private boolean seedDatabase;
+//
+//
+//    Logger logger = LoggerFactory.getLogger(DataLoaderRunner.class);
+//
+//    /* Add repos here and remove templateRepository */
+//    LoonstrokenRepository templateRepository;
+//
+//
+//    public DataLoaderRunner(LoonstrokenRepository templateRepository){
+//        this.templateRepository = templateRepository;
+//    }
+//
+//
+//    private boolean shouldSeed(){
+//        return templateRepository.findAll().size() <= 0;
+//    }
+//
+//
+//    @Override
+//    public void run(ApplicationArguments args)  {
+//        if(!seedDatabase || !shouldSeed())
+//        {
+//            logger.info("Won't seed database, .env property is false or already seeded!");
+//            return;
+//        }
+//
+//        logger.info("Will seed database now!");
+//
+//        Loonstroken newTemplate = new Loonstroken();
+//        newTemplate.setValue("setBySeed!");
+//        templateRepository.save(newTemplate);
+//
+//    }
+//
+//}
