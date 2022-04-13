a28e353ff5695a0f91b9502c5329156d5bde48e1
mohamedikhlaf27 <61310252+mohamedikhlaf27@users.noreply.github.com>
2022-04-13 15:47:24 +0200

    delete project

--- src/main/java/werkportaal/loonstrokenServer/repository/LoonstrokenRepository.java
+++
@@ -1,7 +0,0 @@
-package werkportaal.loonstrokenServer.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import werkportaal.loonstrokenServer.model.Loonstroken;
-
-public interface LoonstrokenRepository extends JpaRepository<Loonstroken, String> {
-}
