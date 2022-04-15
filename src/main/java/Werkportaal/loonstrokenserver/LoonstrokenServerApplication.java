82d8e485ce452aece8e938fb80acc926506bdf78
mohamedikhlaf27 <61310252+mohamedikhlaf27@users.noreply.github.com>
2022-04-15 15:33:15 +0200

    update pipeline.yml

--- src/main/java/Werkportaal/loonstrokenserver/LoonstrokenServerApplication.java
+++ src/main/java/Werkportaal/loonstrokenserver/LoonstrokenServerApplication.java
@@ -2,8 +2,10 @@ package Werkportaal.loonstrokenserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com"})
 public class LoonstrokenServerApplication {
 
 	public static void main(String[] args) {
